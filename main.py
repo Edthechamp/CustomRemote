06acd02a5e246e9bda43e9b222b8c79d0eb17800
Edvards(laptop) <e.ernests.mathers@gmail.com>
2025-05-05 14:58:01 +0300

    basic controls, commented

--- main.py
+++ main.py
@@ -7,21 +7,25 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("home/media")
     client.subscribe("controls/basic")
 
-#for windows
+
 def message(client, userdata, msg):
     command = msg.payload.decode()
     if msg.topic == "home/media":
         media(command)
     elif msg.topic == "controls/basic":
         controls(command)
+    #TODO: add spotify support
 
 def media(command):
     if command == "netflix":
         print("Opening Netflix")
         subprocess.run(['firefox','-kiosk','netflix.com'])
+        #TODO: check if netflix is already open and swithc focus in that case and add a way to close it once done
     elif command == "youtube":
         print("Opening YouTube")
         subprocess.run(['firefox',"https://www.youtube.com"])
+        #TODO: same as netflix
+
 
 def controls(command):
     subprocess.run(['xdotool', 'key', command])
@@ -29,9 +33,11 @@ def controls(command):
 
 #publishing client is phone with mqtt app
 
+#TODO: maybe replace subprocess.run with prebiuilt executable for better performance
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = message
 
-client.connect("192.168.23.105", 1883, 60)
+client.connect("localhost", 1883, 60) #subscribed client is on the same linux machine as the broker
 client.loop_forever()
